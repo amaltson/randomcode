e383efb9795daf66a3ab58df79ca6d19199d1192
Arthur Kalmenson <arthur@ares.local>
2009-12-08 15:12:38 -0500

    * created a new service and an implementation. * added spring MVC and the gwt-af-command pieces.

--- src/main/java/com/sinai/gwttwo/client/Application.java
+++ src/main/java/com/sinai/gwttwo/client/Application.java
@@ -1,6 +1,11 @@
 package com.sinai.gwttwo.client;
 
+import java.util.List;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -13,7 +18,16 @@ public class Application implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Label label = new Label("nothing");
-		RootPanel.get().add(label);
+		GwtServiceAsync gwtService = GWT.create(GwtService.class);
+		gwtService.getStringList(new AsyncCallback<List<String>>() {
+
+			public void onSuccess(List<String> arg0) {
+				Window.alert(arg0.toString());
+			}
+
+			public void onFailure(Throwable arg0) {
+				Window.alert(arg0.toString());
+			}
+		});
 	}
 }
