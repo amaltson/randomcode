cdb298698aaaea48009cc57183810f060cfa03c4
Arthur Kalmenson <arthur@ares.local>
2009-12-11 14:11:18 -0500

    * testing out the externalized CSS feature with <ui:style>

--- src/main/java/com/sinai/gwttwo/client/Application.java
+++ src/main/java/com/sinai/gwttwo/client/Application.java
@@ -1,13 +1,8 @@
 package com.sinai.gwttwo.client;
 
-import java.util.List;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sinai.gwttwo.client.ui.HelloWorld;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -18,16 +13,7 @@ public class Application implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		GwtServiceAsync gwtService = GWT.create(GwtService.class);
-		gwtService.getStringList(new AsyncCallback<List<String>>() {
-
-			public void onSuccess(List<String> arg0) {
-				Window.alert(arg0.toString());
-			}
-
-			public void onFailure(Throwable arg0) {
-				Window.alert(arg0.toString());
-			}
-		});
+		HelloWorld helloWorld = new HelloWorld("world", "hi", "hey");
+		RootPanel.get().add(helloWorld);
 	}
 }
---
+++ src/main/java/com/sinai/gwttwo/client/ui/HelloWorld.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2009. Mount Sinai Hospital, Toronto, Canada.
+ * 
+ * IN NO EVENT SHALL MOUNT SINAI HOSPITAL BE LIABLE TO ANY PARTY FOR DIRECT, 
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST 
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+ * EVEN IF MOUNT SINAI HOSPITAL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+ * DAMAGE.
+ * 
+ * MOUNT SINAI HOSPITAL SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE AND 
+ * ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS". 
+ * MOUNT SINAI HOSPITAL HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, 
+ * UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+ */
+package com.sinai.gwttwo.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class HelloWorld extends Composite {
+
+	private static HelloWorldUiBinder uiBinder =
+			GWT.create(HelloWorldUiBinder.class);
+
+	interface HelloWorldUiBinder extends UiBinder<Widget, HelloWorld> {
+	}
+
+
+	public HelloWorld(String... names) {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
